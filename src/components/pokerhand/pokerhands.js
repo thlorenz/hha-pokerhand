f0a18852d639197ea387260d25f023426005f4ae
Thorsten Lorenz <thlorenz@gmx.de>
2017-02-15 00:38:44 -0500

    select: making hand selectable and adding autoscroll

--- src/components/pokerhand/pokerhands.js
+++ src/components/pokerhand/pokerhands.js
@@ -1,24 +1,45 @@
 const React = require('react')
 const { Component } = React
+const ReactDom = require('react-dom')
 const PokerHand = require('./pokerhand')
 
 class PokerHands extends Component {
   render() {
-    const { hands } = this.props
-    return <div>{(hands || []).map(this._toHand, this)}</div>
+    const { hands = [] } = this.props
+    this._pokerHands = new Array(hands.length)
+    return <div>{(hands).map(this._toHand, this)}</div>
+  }
+
+  componentDidUpdate() {
+    const { selectedIndex, autoScroll = true } = this.props
+    if (!autoScroll) return
+    const el = ReactDom.findDOMNode(this._pokerHands[selectedIndex])
+    el.scrollIntoView({ block: 'start', behavior: 'smooth' })
   }
 
   _toHand(x, idx) {
-    const { onhandSelected, injectHeader, injectFooter } = this.props
+    const { injectHeader, injectFooter, selectedIndex } = this.props
+    const className = selectedIndex === idx
+      ? 'selected'
+      : ''
+
     return (
       <PokerHand
         hand={x}
         key={idx}
-        onselected={onhandSelected}
+        ref={el => (this._pokerHands[idx] = el)}
+        onselected={(hand, component) => this._onhandSelected(hand, component, idx)}
         injectHeader={injectHeader}
         injectFooter={injectFooter}
+        className={className}
       />
     )
+  }
+
+  _onhandSelected(hand, component, idx) {
+    const { onhandSelected } = this.props
+    if (typeof onhandSelected !== 'function') return
+    onhandSelected(hand, component, idx)
   }
 }
 
